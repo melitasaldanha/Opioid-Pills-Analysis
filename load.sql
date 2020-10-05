6c5f3b03b774d296d0b31ed807b364bf9d6524b9
Melita Saldanha <msaldanha@cs.stonybrook.edu>
2020-10-05 02:06:08 -0400

    Uploaded files

---
+++ load.sql
@@ -0,0 +1,2 @@
+load from "/datasets/dea_ny.csv" 
+of del modified by DATEFORMAT="MMDDYYYY" MESSAGES load.txt INSERT INTO CSE532.DEA_NY;
---
+++ zipload.sql
@@ -0,0 +1,3 @@
+load from "/datasets/nyzip.csv" of del 
+modified by DATEFORMAT="MMDDYYYY" 
+MESSAGES zipload.txt INSERT INTO CSE532.ZIPPOP NONRECOVERABLE;
